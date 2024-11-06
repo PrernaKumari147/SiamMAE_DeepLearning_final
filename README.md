bb2b6a7778c46bfc86a82ef3f92fb69bf721ba7f
PrernaKumari147 <Pxk99090@ucmo.edu>
2024-11-06 01:26:02 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# Final Project of Deep Learning - Advanced Course
+
+In this project, we aim to reproduce the Siamese Masked AutoEncoder (SiamMAE) proposed in [Siamese Masked Autoencoders](https://arxiv.org/abs/2305.14344) using the PyTorch framework.
+The dataset we will be pretraining our model will be UCF-101 [UCF101: A Dataset of 101 Human Actions
+Classes From Videos in The Wild](https://www.crcv.ucf.edu/papers/UCF101_CRCV-TR-12-01.pdf). The representation learned by the model will be evaluated on an object segmentation task on DAVIS-2017 [The 2017 DAVIS Challenge on Video Object Segmentation](https://arxiv.org/abs/1704.00675).
+
+This project was part of the course [DD2412 Deep Learning, Advanced Course](https://www.kth.se/student/kurser/kurs/DD2412?l=en) at KTH.
+
+Please refer to the notebook example to see how to run the experiments. We could not upload our trained model to the Github due to storage limiations.
+
+## Results
+Example of results from object segmentation. The first video was an easy example whereas the second was a harder one. For more details, refer to our project report.
+![davis_bear](https://github.com/Jeremylin0904/DeepLearning_final/assets/117983459/0c434532-417e-4509-817a-eba42a35e3e1)
+![davis_bike](https://github.com/Jeremylin0904/DeepLearning_final/assets/117983459/c4ebc99d-a696-4d11-9fa5-4faa339242ec)
---
+++ utils.py
@@ -0,0 +1,65 @@
+import os
+import matplotlib.pyplot as plt
+from LabelPropagation import  read_list_frames, read_list_labels
+from tqdm import tqdm
+import numpy as np 
+from PIL import Image
+from sklearn.metrics import jaccard_score, f1_score
+from glob import glob
+
+def show_segm(videos_path, labels_path, video_name, frame_num, model_name):
+  frame_path = os.path.join(videos_path, video_name, frame_num) + '.jpg'
+  segm_pred_path = os.path.join(videos_path, model_name, video_name, frame_num) + '.png'
+  segm_gt_path = os.path.join(labels_path, video_name, frame_num) + '.png'
+
+  frame = plt.imread(frame_path)
+  segm_pred = plt.imread(segm_pred_path)
+  segm_gt = plt.imread(segm_gt_path)
+
+  fig, axs = plt.subplots(1, 2, figsize=(10,10))
+  axs[0].imshow(frame, cmap='gray')
+  axs[0].imshow(segm_pred, cmap='gray', alpha=0.5)
+  axs[0].title.set_text('predicted segmentation')
+
+  axs[1].imshow(frame, cmap='gray')
+  axs[1].imshow(segm_gt, cmap='gray', alpha=0.5)
+  axs[1].title.set_text('ground truth segmentation')
+
+  fig.show()
+
+def score_one_vid(videos_path, labels_path, video_name, model_name):
+  list_segs = glob(os.path.join(videos_path, model_name, video_name, '*.png'))
+  list_labels = read_list_labels(os.path.join(labels_path, video_name))
+  n = len(list_segs)
+  scores = np.zeros((n-1,2))
+  for i in tqdm(range(1, n)):
+    pred_seg = Image.open(list_segs[i])
+    real_seg = Image.open(list_labels[i])
+    pred_seg = np.asarray(pred_seg).reshape(-1)
+    real_seg = np.asarray(real_seg).reshape(-1)
+    scores[i-1,0] = jaccard_score(real_seg, pred_seg, average='macro')
+    scores[i-1,1] = f1_score(real_seg, pred_seg, average='macro')
+
+  return np.mean(scores, axis=0)
+
+def eval_model(list_videos, videos_path, labels_path, model_name):
+  scores = []
+  for video in tqdm(list_videos):
+    list_segs = glob(os.path.join(videos_path, model_name, video, '*.png'))
+    list_labels = read_list_labels(os.path.join(labels_path, video))
+    n = len(list_segs)
+    scores_vid = np.zeros((n-1,2))
+    for i in range(1, n):
+      pred_seg = Image.open(list_segs[i])
+      real_seg = Image.open(list_labels[i])
+      pred_seg = np.asarray(pred_seg).reshape(-1)
+      real_seg = np.asarray(real_seg).reshape(-1)
+      scores_vid[i-1,0] = jaccard_score(real_seg, pred_seg, average='macro')
+      scores_vid[i-1,1] = f1_score(real_seg, pred_seg, average='macro')
+    scores.append(scores_vid)
+  scores = np.concatenate(scores, axis=0)
+  scores = np.mean(scores, axis=0)
+  print(f'J = {scores[0]}, F = {scores[1]}, J&F = {np.mean(scores)}')
+  return scores
+
+
